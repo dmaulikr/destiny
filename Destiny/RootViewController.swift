e95dd2df57ef9991a484d30aff25566258a4e31f
Thomas Castiglione <castiglione@gmail.com>
2014-06-22 01:27:59 +0800

    async audio (maybe a bad idea!)

--- Destiny/RootViewController.swift
+++ Destiny/RootViewController.swift
@@ -1,9 +1,11 @@
 import UIKit
 import AVFoundation
 
-@objc(RootViewController) class RootViewController: UIViewController {
+@objc(RootViewController) class RootViewController: UIViewController, AVAudioPlayerDelegate {
     
     let config: Settings
+    let queue: dispatch_queue_t
+    let audio_finished: dispatch_semaphore_t
     let player: AVAudioPlayer
     
     @IBOutlet var diceView: DiceViewController
@@ -11,11 +13,16 @@ import AVFoundation
     init(coder: NSCoder?) {
         config = Settings()
         
+        queue = dispatch_queue_create("audio", nil)
+        audio_finished = dispatch_semaphore_create(0)
+        
         let soundPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("dice", ofType: "wav"))
         player = AVAudioPlayer(contentsOfURL: soundPath, error: nil)
         player.volume = 1.0
         
         super.init(coder: coder)
+        
+        player.delegate = self
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject?) {
@@ -34,8 +41,15 @@ import AVFoundation
     
     @IBAction func roll(sender: AnyObject!) {
         if (config.sound) {
-            player.play()
+            dispatch_async(queue) {
+                self.player.play()
+                dispatch_semaphore_wait(self.audio_finished, DISPATCH_TIME_FOREVER)
+            }
         }
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        dispatch_semaphore_signal(audio_finished)
     }
     
 }
